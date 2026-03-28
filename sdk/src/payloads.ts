5a614e39415beb15813eeeaad4d8f8cc94d240f0
winsznx <timjosh507@gmail.com>
2026-03-28 18:20:41 +0100

    feat(types): export fundamental Pledge signature types

--- sdk/src/payloads.ts
+++ sdk/src/payloads.ts
@@ -4,3 +4,7 @@ export interface CreateCampaignPayload {
   goalMicroStx: number;
   timelockBlockHeight: number;
 }
+export interface PledgePayload {
+  campaignId: string;
+  amountMicroStx: number;
+}
