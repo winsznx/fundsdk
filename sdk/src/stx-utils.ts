c31cb198bdf93270eae5f8686d59ea21284108da
winsznx <timjosh507@gmail.com>
2026-03-28 18:36:41 +0100

    feat(utils): integrate native B58 validation checking patterns securely

--- sdk/src/stx-utils.ts
+++ sdk/src/stx-utils.ts
@@ -1,2 +1,3 @@
 export const microStxToStx = (micro: number) => micro / 1000000;
 export const stxToMicroStx = (stx: number) => Math.floor(stx * 1000000);
+export const isValidPrincipal = (addr: string) => /^(S[PM])[a-zA-Z0-9]{37,39}$/.test(addr);
