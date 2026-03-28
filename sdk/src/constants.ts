879924f97ac7c1b8d5549c615fb58d75122d89b7
winsznx <timjosh507@gmail.com>
2026-03-28 18:28:46 +0100

    chore(constants): define clarion local machine testing node strings

--- sdk/src/constants.ts
+++ sdk/src/constants.ts
@@ -1,2 +1,3 @@
 export const STACKS_MAINNET_API_URL = "https://api.mainnet.hiro.so";
 export const STACKS_TESTNET_API_URL = "https://api.testnet.hiro.so";
+export const STACKS_DEVNET_API_URL = "http://localhost:3999";
