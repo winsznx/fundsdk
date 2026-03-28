371992c74b1452a08c0e7c8f5026df240795bedb
winsznx <timjosh507@gmail.com>
2026-03-28 18:52:33 +0100

    feat(events): model distinct pledge event emission shapes natively

--- sdk/src/events.ts
+++ sdk/src/events.ts
@@ -2,3 +2,8 @@ export interface FundotEvent {
   eventId: string;
   contractId: string;
 }
+export interface PledgeEvent extends FundotEvent {
+  type: "PLEDGE";
+  amount: number;
+  pledger: string;
+}
