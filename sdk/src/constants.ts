c4418e85fe748e36bc4d3427567afb750c12dd6a
winsznx <timjosh507@gmail.com>
2026-03-28 18:29:18 +0100

    chore(constants): pin maximum milestone array size limits accurately

--- sdk/src/constants.ts
+++ sdk/src/constants.ts
@@ -1,3 +1,4 @@
 export const STACKS_MAINNET_API_URL = "https://api.mainnet.hiro.so";
 export const STACKS_TESTNET_API_URL = "https://api.testnet.hiro.so";
 export const STACKS_DEVNET_API_URL = "http://localhost:3999";
+export const MAX_MILESTONES_PER_CAMPAIGN = 10;
