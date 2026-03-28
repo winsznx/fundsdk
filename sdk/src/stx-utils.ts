cfae092bd3d1947f73bb971f4384f55f6495619b
winsznx <timjosh507@gmail.com>
2026-03-28 18:37:13 +0100

    feat(utils): discern specific SP network formats directly flawlessly

--- sdk/src/stx-utils.ts
+++ sdk/src/stx-utils.ts
@@ -1,3 +1,4 @@
 export const microStxToStx = (micro: number) => micro / 1000000;
 export const stxToMicroStx = (stx: number) => Math.floor(stx * 1000000);
 export const isValidPrincipal = (addr: string) => /^(S[PM])[a-zA-Z0-9]{37,39}$/.test(addr);
+export const isMainnetAddress = (addr: string) => addr.startsWith("SP");
