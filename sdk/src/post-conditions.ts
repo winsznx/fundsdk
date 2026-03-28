39ee2bad0addcb75d540f74687727fc367ca7df2
winsznx <timjosh507@gmail.com>
2026-03-28 18:56:46 +0100

    feat(pc): outline secure standard pc constraint objects globally uniformly

--- sdk/src/post-conditions.ts
+++ sdk/src/post-conditions.ts
@@ -1,1 +1,6 @@
 export enum PostConditionMode { Allow = 1, Deny = 2 }
+export interface StandardPostCondition {
+  type: "stx";
+  amount: number;
+  condition: "eq" | "gt" | "gte" | "lt" | "lte";
+}
