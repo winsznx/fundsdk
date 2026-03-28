35951b52910f75774b92c6625ee48f15e633730f
winsznx <timjosh507@gmail.com>
2026-03-28 18:29:50 +0100

    chore(constants): setup 0.1 STX minimum investment boundary constraints

--- sdk/src/constants.ts
+++ sdk/src/constants.ts
@@ -2,3 +2,4 @@ export const STACKS_MAINNET_API_URL = "https://api.mainnet.hiro.so";
 export const STACKS_TESTNET_API_URL = "https://api.testnet.hiro.so";
 export const STACKS_DEVNET_API_URL = "http://localhost:3999";
 export const MAX_MILESTONES_PER_CAMPAIGN = 10;
+export const MINIMUM_PLEDGE_MICRO_STX = 100000;
